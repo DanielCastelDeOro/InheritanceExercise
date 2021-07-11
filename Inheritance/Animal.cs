f1592fb7b1e76bbf9efd740c13946699ccf72023
Daniel Castel De Oro <83740100+DanielCastelDeOro@users.noreply.github.com>
2021-07-11 05:10:55 -0600

    Final commit

---
+++ Inheritance/Animal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    class Animal
+    {
+        public bool Teeth { get; set; }
+        public bool Eyes { get; set; }
+        public bool Ears { get; set; }
+        public bool Parents { get; set; }
+    }
+
+    class Birds : Animal
+    {
+        public bool Wings { get; set; }
+        public bool beak { get; set; }
+        public bool layEggs { get; set; }
+        public bool hasFeather { get; set; }
+    }
+
+    class Reptiles : Animal
+    {
+        public bool hasScales { get; set; }
+        public bool isColdBlooded { get; set; }
+        public bool hasVertebrates { get; set; }
+        public bool haveAtLeastOneLung { get; set; }
+    }
+}
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -6,6 +6,24 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
+        Birds Birds = new Birds(); 
+            Birds.Wings = true;
+            Birds.hasFeather = true;
+            Birds.layEggs = true;
+            Birds.beak = true;
+            
+            Console.WriteLine($"All Birds have Wings: {Birds.Wings}\nAll Birds have Feathers: {Birds.hasFeather}\nAll Birds have a Beak: {Birds.beak}\nAll Birds lay Eggs: {Birds.layEggs}");
+            Console.WriteLine("------------------------------------------------------");
+
+        Reptiles reptiles = new Reptiles();
+            reptiles.hasVertebrates = true;
+            reptiles.haveAtLeastOneLung = true;
+            reptiles.hasScales = true;
+            reptiles.isColdBlooded = true;
+
+            Console.WriteLine($"All reptiles have vertebrates: {reptiles.hasVertebrates}\nAll reptiles have at least one lung: {reptiles.haveAtLeastOneLung}\nAll reptiles have scales: {reptiles.hasScales}\nAll reptiles are cold blooded: {reptiles.isColdBlooded}");
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("Have a sssssplendid day!!!");
             // TODO Be sure to follow best practice when creating your classes
 
             // Create a class Animal
